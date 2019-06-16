5d23962555bd361e93afdd208250b8f5118b15f0
ma-za-kpe <makpalyy@gmail.com>
2019-06-16 13:33:48 +0200

    added partials folder to the view folder

--- API/routes/index.js
+++ API/routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+
+// home is protected
+router.get('/home', (req, res) =>
+  res.render('home', {
+    // name: req.user.name
+  })
+);
+
+// login is protected
+router.get('/login', (req, res) =>
+  res.render('login', {
+    // name: req.user.name
+  })
+);
+
+// register is protected
+router.get('/register', (req, res) =>
+  res.render('register', {
+    // name: req.user.name
+  })
+);
 
 module.exports = router;
